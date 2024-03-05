4ce622c3ad6e0c0dbd8c8d4fc3c2fc6b8e6e1b03
dmorales2022171 <dmorales-2022171@kinal.edu.gt>
2024-03-05 17:13:18 -0600

    se agrego la funcion de crear un usuario determinado

--- configs/mongo.js
+++ configs/mongo.js
@@ -1,34 +1,51 @@
-'use strict'
-
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
+import User from "../src/users/user.model.js";
 
 export const dbConnection = async () => {
     try {
         mongoose.connection.on('error', () => {
-            console.log('MongoDB | could not be connect to mongodb')
+            console.log('MongoDB | could not connect to MongoDB');
             mongoose.disconnect();
-        })
+        });
+
         mongoose.connection.on('connecting', () => {
-            console.log('MongoDB | Try connecting');
-        })
-        mongoose.connection.on('connected', () => {
-            console.log('MongoDB | connected to mongoDB');
-        })
+            console.log('MongoDB | Trying to connect');
+        });
+
+        mongoose.connection.on('connected', async () => {
+            console.log('MongoDB | Connected to MongoDB');
+
+            // Verificar si ya existe un usuario administrador
+            const adminUser = await User.findOne({ role: 'ADMIN_ROLE' });
+            if (!adminUser) {
+                // Crear un usuario administrador predeterminado
+                await User.create({
+                    name: 'Admin',
+                    mail: 'admin@example.com',
+                    password: 'adminPassword', // ¡Recuerda cambiar esto por una contraseña segura!
+                    role: 'ADMIN_ROLE'
+                });
+                console.log('Default admin user created successfully');
+            }
+        });
+
         mongoose.connection.on('open', () => {
-            console.log('MongoDB | connected to database')
-        })
+            console.log('MongoDB | Connected to database');
+        });
+
         mongoose.connection.on('reconnected', () => {
-            console.log('MongoDB | reconnected to MongoDB')
-        })
+            console.log('MongoDB | Reconnected to MongoDB');
+        });
+
         mongoose.connection.on('disconnected', () => {
-            console.log('MongoDB | disconnected')
-        })
+            console.log('MongoDB | Disconnected');
+        });
 
         await mongoose.connect(process.env.URI_MONGO, {
             serverSelectionTimeoutMS: 5000,
             maxPoolSize: 50
         });
-    } catch (e) {
-        console.log('Database connection failed', e)
+    } catch (error) {
+        console.log('Database connection failed', error);
     }
-}
+};
